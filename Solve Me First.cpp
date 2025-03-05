71dd188add592e5ae7b88d543ac0feb30d4942e4
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-03-05 09:13:09 +0600

    Create Solve Me First.cpp

---
+++ Solve Me First.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int solveMeFirst(int a,int b){
+    return a+b;
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int x=solveMeFirst(a,b);
+    cout<<x<<endl;
+    return 0;
+}
